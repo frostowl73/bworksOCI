c513b771d04e18b30a1169d3690b977a70429aeb
Led Zepplin <frostowl73@gmail.com>
2024-01-16 14:30:28 -0700

    jan 16 commit

--- Base_oci.py
+++ Base_oci.py
@@ -16,7 +16,7 @@ import os
 
 # configure the API, connect and authenticate to the server
 api = BroadworksAPI(
-    host = 'xxx', port='2208', username='xxx@vle.broadsoft.com', password='xxx',
+    host = '10.122.51.167', port='2208', username='jester@vle.broadsoft.com', password='happy123',
 )
 
 # get the platform software level
@@ -26,11 +26,12 @@ print('\n')
 
 sysadmin = api.command("SystemAdminGetListRequest")
 for admin in sysadmin.system_admin_table:
-    print(admin.administrator_id)
+    print(admin.administrator_id, admin.first_name)
 #print(sysadmin.system_admin_table) 
 print('\n')
 
 sp_list = api.command("ServiceProviderGetListRequest")
 for provider in sp_list.service_provider_table:
         print(provider.service_provider_id)
-#print(sp_list.service_provider_table)
+print(sp_list.service_provider_table)
+
